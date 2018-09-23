f810303094061ed169fc4be7e0649df813e09129
Gonçalo Fonseca <1150503@isep.ipp.pt>
2018-09-23 19:52:29 +0100

    Ex07

--- modulo0/ex07/main.c
+++ modulo0/ex07/main.c
@@ -2,33 +2,30 @@
 
 
 char* integer_base(char *vec){
-	
-	int binario  = 1, octal = 1,  hexa =1 ;
-	int i = 0;
+
+	int binario  = 1, octal = 1,  hexa = 1, i = 0;
+
 	while(vec[i] != 0){
 
 		if(vec[i] > '1') binario = 0;
 		if(vec[i] > '7') octal = 0;
 		if(vec[i] > '9') return "Hexadecimal";
 
-
-		i++;
-		}
-		if (binario) return "Binario";
-		if(octal ) return "Octal";
-		return "Decimal";
-
-
+    i++;
+	 }
+    if (binario) return "\nBinario";
+		if (octal) return "\nOctal";
+		return "\nDecimal";
 	}
 
-int main(void){
+int main(){
 
+  char num[20];
 
-  //Exercicio 7
- fgets(num1,10,num1);
+  printf("Número\n");
+  fgets(num,20,stdin);
 
- printf("%s",integer_base(num1));
+  printf("%s\n",integer_base(num));
 
-
-
+  return 0;
 }

f53f598e492807ab649007b754d5bcd81658cc06
pedro <1161071@isep.ipp.pt>
2018-10-07 23:11:45 +0100

    ex13Update

--- modulo1/ex13/frequencies.c
+++ modulo1/ex13/frequencies.c
@@ -1,7 +1,15 @@
 void frequencies(float *grades, int n, int *freq)
 {
     int i=0;
+    int j=0;
+    int a=0;
+    for(j;j<=20;j++){
+        a=0;
+        i=0;
     for(i;i<n;i++){
-        *(freq+i) = (int) *(grades+i);
+        if((int) *(grades+i) == j)
+        a++;
+    }
+        *(freq+j)=a;
     }
 }
--- modulo1/ex13/main.c
+++ modulo1/ex13/main.c
@@ -4,13 +4,11 @@ int main()
 {
     int i;
     float grades[] = {8.23, 12.25, 16.45, 12.45, 10.05, 6.45, 14.45, 0.0, 12.67, 16.23, 18.75} ;
-    int freq[11];
+    int freq[20];
     frequencies(grades,11,freq);
     i=0;
-    for(i;i<11;i++){
+    for(i;i<=20;i++){
         printf("%d\n" , freq[i]);
     }
     return 0;
 }
-
-

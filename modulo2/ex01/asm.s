5b06d1475bcd545e354567a52dd8422626ed9cbb
Gonçalo Fonseca <1150503@isep.ipp.pt>
2018-10-19 11:19:42 +0100

    Ex01 done

--- modulo2/ex01/asm.s
+++ modulo2/ex01/asm.s
@@ -1,18 +1,22 @@
+.section .data
   .global op1
   .global op2
   .global res
-  .global sum
-sum:
-		#prologue
-		pushl %ebp
-		movl %esp,%ebp # the statc frame pointer for sum function
-		movl op1, %ebx #place op1 in ebx
-		movl op2, %eax #place op2 in eax
-		addl %ebx,%eax #add ebx to eax. Result is in eax
-#void sum(void)
-# save previous stack frame pointer
-movl %eax, res #copy result to res
-	#epilogue
-	movl %ebp,%esp # restore the previous stack pointer ("clear" the stack)
-	popl %ebp #restore the previous stack prom the pointer
-	ret
+.section .text
+  .global sum # void sum(void)
+
+  sum:
+    # prologue
+    pushl %ebp # save previous stack frame pointer
+    movl %esp, %ebp # the stack frame pointer for sum function
+
+    movl op1, %ecx # place op1 in ecx
+    movl op2, %eax # place op2 in eax
+    addl %ecx, %eax # add ecx to eax. Result is in eax
+
+    movl %eax, res # copy the result to res
+
+    # epilogue
+    movl %ebp, %esp # restore the previous stack pointer ("clear" the stack)
+    popl %ebp # restore the previous stack frame pointer
+    ret

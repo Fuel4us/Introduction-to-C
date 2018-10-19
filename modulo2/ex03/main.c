5e22df3b064f34c553325da503101e79000bd19a
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-10-19 16:15:58 +0100

    :rocket: ex05 ex03

--- modulo2/ex03/main.c
+++ modulo2/ex03/main.c
@@ -5,16 +5,16 @@ int op1=0, op2=0, op3=0, op4=0, res=0;
 
 int main(void) {
 
-  printf("Valor op1");
+  printf("Valor op1: ");
   scanf("%d", &op1);
 
-  printf("Valor op2");
+  printf("Valor op2: ");
   scanf("%d", &op2);
 
-  printf("Valor op3");
+  printf("Valor op3: ");
   scanf("%d", &op3);
 
-  printf("Valor op4");
+  printf("Valor op4: ");
   scanf("%d", &op4);
 
   operation();

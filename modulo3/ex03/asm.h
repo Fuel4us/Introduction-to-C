8d9b20ab1f0eca86caf98c0d08a8617bc2b9c44d
1161071 <1161071@isep.ipp.pt>
2018-11-04 10:22:01 +0000

    ex03

---
+++ modulo3/ex03/asm.h
@@ -0,0 +1,3 @@
+void str_copy_p2();
+extern char *ptr1, *ptr2;
+
---
+++ modulo3/ex03/asm.s
@@ -0,0 +1,58 @@
+.section .data
+  .comm ptr1,4
+  .comm ptr2,4
+  .global ptr1
+  .global ptr2
+
+.section .text
+  .global str_copy_p2
+
+str_copy_p2:
+  # prologue
+  pushl %ebp # save previous stack frame pointer
+  movl %esp, %ebp # the stack frame pointer for sum function
+
+  pushl %esi
+  pushl %edi
+
+  movl ptr1, %esi
+  movl ptr2, %edi
+
+  loop_start:
+
+    movb (%esi), %bl # moves the string in ebx to bl
+    cmp $0, %bl # to see when the string ends
+    je end
+
+    cmp $48, %bl # compares 0 with string using ASCII
+    je change_numbers
+
+    cmp $56, %bl
+    je change_numbers1
+
+    movb (%esi), %cl
+    movb %cl, (%edi)
+    jmp loop_end
+
+  change_numbers:
+
+    movb $56, %al
+    movb %al, (%edi)
+    jmp loop_end
+
+   change_numbers1:
+    movb $48, %al
+    movb %al, (%edi)
+    jmp loop_end
+
+  loop_end:
+    incl %esi
+    incl %edi
+    jmp loop_start
+
+end:
+  # epilogue
+  movl %ebp, %esp # restore the previous stack pointer ("clear" the stack)
+  popl %ebp # restore the previous stack frame pointer
+
+  ret
